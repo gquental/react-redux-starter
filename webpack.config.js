5aa5c06e185f70424894be4e86298a7479849d20
Guilherme Quental <guilherme@quent.al>
2016-12-07 21:58:23 -0200

    Add CSS webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractCSS = new ExtractTextPlugin({ filename: 'bundle.css', disable: false, allChunks: true });
 
 module.exports = {
   entry: {
@@ -16,7 +19,15 @@ module.exports = {
         options: {
           presets: ["es2015", "react", "stage-2"]
         }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: extractCSS.extract("css-loader")
       }
     ]
-  }
+  },
+  plugins: [
+    extractCSS
+  ]
 }
